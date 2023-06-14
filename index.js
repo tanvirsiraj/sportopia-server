68fce75b12db94c678220b97b6e4e5c3662d5ed2
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2023-06-14 01:13:48 -0700

    get method for users

--- index.js
+++ index.js
@@ -38,12 +38,17 @@ async function run() {
     const usersCollection = client.db("sportopiaDb").collection("users");
 
     // users related apis
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log(user);
+      // console.log(user);
       const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-      console.log("existing user", existingUser);
+      // console.log("existing user", existingUser);
       if (existingUser) {
         return res.send({ message: "user already exists" });
       }
